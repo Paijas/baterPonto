8ce9f6ade07cb7d95555765be04070c4103b9269
Matheus Paigel <90693522+Paijas@users.noreply.github.com>
2024-12-10 11:49:43 -0300

    Definindo rota das paginas

--- client/app/index.js
+++ client/app/index.js
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function Index() {
+// Importando as Páginas
+import Login from './pages/login';
+import Home from './pages/home';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <View className='flex-1 justify-center items-center bg-slate-800'>
-      <Text className='text-white text-4xl font-semibold'>Olá, Mundo!</Text>
-    </View>
+
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{ headerShown: false }} 
+        />
+        
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            title: 'Página Inicial',
+            headerStyle: { backgroundColor: '#1e293b' },
+            headerTintColor: '#fff',
+            headerTitleStyle: { fontWeight: 'bold' }
+          }}
+        />
+      </Stack.Navigator>
+
   );
 }
