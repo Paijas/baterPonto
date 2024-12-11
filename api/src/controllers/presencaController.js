e92684a79438a85aba4022ff54498e0797375e9b
spoycrab <lucaslaranja2010@hotmail.com>
2024-12-11 12:54:34 -0300

    New: model presenca

--- api/src/controllers/presencaController.js
+++ api/src/controllers/presencaController.js
@@ -1,21 +1,40 @@
-const presencaModel = require("../models/presencaModel")
-const usuarioModel = require("../models/usuarioModel")
+const presencaModel = require("../models/presencaModel");
+const usuarioModel = require("../models/usuarioModel");
 
-const checkIn = async (req, res) =>{
-    const { usuarioId } = req.body;
-    const dataAtual = new Date();
+const registrarCheckin = async (req, res) => {
+  const { usuarioId } = req.body;
+  const dataAtual = new Date();
 
-    try {
-        const buscarUser = usuarioModel.buscarUser( usuarioId );
-    
-        if (!existeUser){
-            return res.status(400).json({ message: "Usuário não encontrado"})
-        }
-        
-        const presenca = presencaModel.registrarPresenca(existeUser.id)
-    } catch (error) {
-        
+  try {
+    const buscarUser = await usuarioModel.buscarUser(usuarioId);
+    if (!buscarUser) {
+      return res.status(400).json({ message: "Usuário não encontrado" });
     }
-}
 
-module.exports = {checkIn}
+    const presenca = presencaModel.registrarPresenca(buscarUser.id, dataAtual);
+  } catch (error) {
+    return res.status(500).json({ message: "Erro ao fazer checkin", error });
+  }
+};
+
+const registrarAlmocoSaida = async (req, res) => {
+  const { usuarioId } = req.body;
+  const dataAtual = new Date();
+};
+
+const registrarAlmocoVolta = async (req, res) => {
+  const { usuarioId } = req.body;
+  const dataAtual = new Date();
+};
+
+const registrarCheckout = async (req, res) => {
+  const { usuarioId } = req.body;
+  const dataAtual = new Date();
+};
+
+module.exports = {
+  registrarCheckin,
+  registrarCheckout,
+  registrarAlmocoSaida,
+  registrarAlmocoVolta,
+};
--- api/src/models/presencaModel.js
+++ api/src/models/presencaModel.js
@@ -1,17 +1,54 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const registrarPresenca = async (usuarioId, dataAtual) => {
+const buscarRegistroDia = async (usuarioId, data) => {
+  return await prisma.presenca.findFirst({
+    where: { usuarioId: usuarioId, data: data },
+  });
+};
+
+const registrarCheckin = async (usuarioId, horas) => {
   return await prisma.presenca.create({
     data: {
       usuarioId,
-      data: dataAtual,
-      entrada: dataAtual,
+      data: horas,
+      entrada: horas,
       saida: null,
       almocoSaida: null,
       almocoVolta: null,
     },
   });
 };
+const registrarAlmocoSaida = async (registroId, horas) => {
+  return await prisma.presenca.update({
+    where: { id: registroId },
+    data: {
+      almocoSaida: horas,
+    },
+  });
+};
+const registrarAlmocoVolta = async (registroId, horas) => {
+  return await prisma.presenca.update({
+    where: { id: registroId },
+    data: {
+      almocoVolta: horas,
+    },
+  });
+};
+const registrarCheckout = async (registroId, horas) => {
+  return await prisma.presenca.update({
+    where: { id: registroId },
+    data: {
+      saida: horas,
+    },
+  });
+};
 
-module.exports = { registrarPresenca };
+
+module.exports = {
+  registrarCheckin,
+  registrarCheckout,
+  registrarAlmocoSaida,
+  registrarAlmocoVolta,
+  buscarRegistroDia,
+};
